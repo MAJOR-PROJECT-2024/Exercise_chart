db60508bd1a82a21d9ab08564f92ef34791a1bbe
Pooja-Gupta07 <poojagupta7775@gmail.com>
2023-08-11 12:49:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/excerciseaplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.excerciseaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
